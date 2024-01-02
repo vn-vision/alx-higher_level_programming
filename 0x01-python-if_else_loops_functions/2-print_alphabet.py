35e6f6fc737962f84764f4b5e3e8ff200674ed7b
vn-vision <vctrngang@gmail.com>
2024-01-02 10:02:20 -0800

    alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-#chr() converts character from ASCII unicode e.g 97 to 'a'
+# chr() converts character from ASCII unicode e.g 97 to 'a'
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+# chr() -converts the Unicode for ASCII back to characters
+# ord() -gets the UNICODE for each character in ASCII
+
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
-
-#chr() -converts the Unicode for ASCII back to characters
-#ord() -gets the UNICODE for each character in ASCII
+        print("{}".format(chr(letter)), end='')
