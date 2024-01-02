aaf8dbda57acf7c80704c7bce0e586539d92d730
vn-vision <vctrngang@gmail.com>
2024-01-02 09:46:42 -0800

    alphabets

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end='')
+#ord() -gets the unicode of a character in ASCII
+#chr() -converts the unicode back to characters
