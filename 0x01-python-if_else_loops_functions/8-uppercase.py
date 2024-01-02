440faeadb90d0ee18d7068136811391bed10f1d8
vn-vision <vctrngang@gmail.com>
2024-01-02 10:50:16 -0800

    error unknown

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    # prints a string in uppercase
+    # string in uppercase
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
