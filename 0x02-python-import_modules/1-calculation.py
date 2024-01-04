c78dd852d59096c63157635be247a8effd493cb5
vn-vision <vctrngang@gmail.com>
2024-01-04 03:32:40 -0800

    working

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
 
-    from calculator_1.py import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     print(f"{a} + {b} = {add(a, b)})")
     print("{} - {} = {})".format(a, b, sub(a, b)))
     print("{} * {} = {})".format(a, b, mul(a, b)))
-    print("{} \\ {} = {})".format(a, b, div(a, b)))
+    print("{} / {} = {})".format(a, b, div(a, b)))
