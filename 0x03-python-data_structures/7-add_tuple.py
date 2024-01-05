9ce2a8b5e8fadc65285aaf63c2746fdb61a3036b
vn-vision <vctrngang@gmail.com>
2024-01-05 08:59:08 -0800

    tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    argA = len(tuple_a)
+    argB = len(tuple_b)
+
+    if argA >= 2:
+        x_a, x_b = tuple_a[0], tuple_a[1]
+    else:
+        x_a, x_b = tuple_a if argA >= 1 else 0, 0
+
+    if argB >= 2:
+        y_a, y_b = tuple_b[0], tuple_b[1]
+    else:
+        y_a, y_b = tuple_b[0] if argB >= 1 else 0, 0
+
+    x = x_a + y_a
+    y = x_b + y_b
+
+    return (x, y)
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    ln = len(sentence)
+
+    f_chr = sentence[0]
+
+    return (ln, f_chr)
