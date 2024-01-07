a07b28b1f957f19466b25d0e087d7a665271d396
vn-vision <vctrngang@gmail.com>
2024-01-07 11:57:20 -0800

    Added prototype

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,61 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
+ * is_palindrome - checks whether a list is a palindrome
+ * @head: point to first elements pointer
+ * Return: 1 if palindrome, else 0
+ * empty list is considered a palindrome
+ * consider two people fast and slow reading through list
+ * the fast one read two nums at a time and the slow 1
+ * by the time the faster one finishes the other will be
+ * halfway through.
+ * reverse the second half from the middle
+ * compare the first half and the second
+ * if thwy match totally it is a palindrome else not
  */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
 
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+        listint_t *fast, *slow, *temp, *second_half;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+        temp = malloc(sizeof(listint_t));
+        if (temp == NULL)
+                return (0);
 
-	while (1)
-	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
-	}
+        if (*head == NULL || (*head)->next == NULL)
+                return (1);
 
-	reverse_listint(&dup);
+        fast = *head;
+        slow = *head;
+        second_half = NULL;
 
-	while (dup && temp)
-	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
-			return (0);
-	}
+        while (fast != NULL && fast->next != NULL)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+        }
 
-	if (!dup)
-		return (1);
+        /**
+         * separater the two halves
+         */
+        temp->next = NULL;
 
-	return (0);
+        while (slow != NULL)
+        {
+                temp = slow->next;
+                slow->next = second_half;
+                second_half = slow;
+                slow = temp;
+        }
+        while (*head != NULL && second_half != NULL)
+        {
+                if ((*head)->n != second_half->n)
+                        return (0);
+
+                *head = (*head)->next;
+                second_half = second_half->next;
+        }
+        return (1);
 }
