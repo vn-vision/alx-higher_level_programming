ff9143f8ebf275dd6225b4a9cad9a1abe01eef99
vn-vision <vctrngang@gmail.com>
2024-01-07 11:43:39 -0800

    This is C

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -18,44 +18,44 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *fast, *slow, *temp, *second_half;
+        listint_t *fast, *slow, *temp, *second_half;
 
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-		return (0);
+        temp = malloc(sizeof(listint_t));
+        if (temp == NULL)
+                return (0);
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+        if (*head == NULL || (*head)->next == NULL)
+                return (1);
 
-	fast = *head;
-	slow = *head;
-	second_half = NULL;
+        fast = *head;
+        slow = *head;
+        second_half = NULL;
 
-	while (fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-	}
+        while (fast != NULL && fast->next != NULL)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+        }
 
-	/**
-	 * separater the two halves
-	 */
-	temp->next = NULL;
+        /**
+         * separater the two halves
+         */
+        temp->next = NULL;
 
-	while (slow != NULL)
-	{
-		temp = slow->next;
-		slow->next = second_half;
-		second_half = slow;
-		slow = temp;
-	}
-	while (*head != NULL && second_half != NULL)
-	{
-		if ((*head)->n != second_half->n)
-			return (0);
+        while (slow != NULL)
+        {
+                temp = slow->next;
+                slow->next = second_half;
+                second_half = slow;
+                slow = temp;
+        }
+        while (*head != NULL && second_half != NULL)
+        {
+                if ((*head)->n != second_half->n)
+                        return (0);
 
-		*head = (*head)->next;
-		second_half = second_half->next;
-	}
-	return (1);
+                *head = (*head)->next;
+                second_half = second_half->next;
+        }
+        return (1);
 }
