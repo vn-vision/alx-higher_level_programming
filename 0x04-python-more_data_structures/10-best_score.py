7965199d562afcbd95ffd00d02f8af3eb3ca9aa7
vn-vision <vctrngang@gmail.com>
2024-01-10 04:33:49 -0800

    a different max checker

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@
 def best_score(a_dictionary):
 
     if a_dictionary:
-        return max(a_dictionary.keys())
+        return max(a_dictionary, key=a_dictionary.get)
     else:
         return None
