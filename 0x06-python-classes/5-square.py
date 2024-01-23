f0da3140545a0af15a7e1ad89ebb10c55635ceb0
vn-vision <vctrngang@gmail.com>
2024-01-24 01:51:59 +0300

    Adding documentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,31 +5,39 @@ uses property to update and access private attributes """
 
 
 class Square:
+	""" Class square and it's attributes """
+	
 	def __init__(self, size=0):
+		""" private instance size """
+		
 		if not isinstance(size, int):
 			raise TypeError("size must be an integer")
 		elif size < 0:
 			raise ValueError("size must be >= 0")
 		self.__size = size
-	""" the property getter and setter for size """
+		
 	@property
 	def size(self):
+		""" the property getter and setter for size """
 		return self.__size
 
 	@size.setter
 	def size(self, value):
+		""" the setter for the private instance """
+		
 		if not isinstance(value, int):
 			raise TypeError("size must be an integer")
 		elif value < 0:
 			raise ValueError("size must be >= 0")
 		self.__size = value
 
-	""" public function to calculate the area of the square """
 	def area(self):
+		""" public function to calculate the area of the square """
 		res = self.__size ** 2
 		return res
 
 	def my_print(self):
+	""" my_print function - prints the square in '#' """
 		if self.__size == 0:
 			print()
 		else:
