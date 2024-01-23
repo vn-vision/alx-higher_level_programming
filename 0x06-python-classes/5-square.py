a7d8c816daa1dfe826abdaf206233020e1230bd2
vn-vision <vctrngang@gmail.com>
2024-01-24 01:57:05 +0300

    Adding documentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,26 +5,41 @@ uses property to update and access private attributes """
 
 
 class Square:
-	""" Class square and it's attributes """
-	
+	"""
+	Class that defines properties of square by: (based on 4-square.py).
+
+	Attributes:
+	size: size of a square (1 side).
+	"""
 	def __init__(self, size=0):
-		""" private instance size """
-		
+		"""Creates new instances of square.
+
+		Args:
+		    size: size of the square (1 side).
+		"""
 		if not isinstance(size, int):
 			raise TypeError("size must be an integer")
 		elif size < 0:
 			raise ValueError("size must be >= 0")
-		self.__size = size
-		
+		else:
+			self.__size = size
+
 	@property
 	def size(self):
-		""" the property getter and setter for size """
+		""" Return the size """
 		return self.__size
 
 	@size.setter
 	def size(self, value):
-		""" the setter for the private instance """
-		
+		"""Property setter for size.
+
+		Args:
+		    value (int): size of a square (1 side).
+
+		Raises:
+		    TypeError: size must be an integer
+		    ValueError: size must be >= 0
+		"""
 		if not isinstance(value, int):
 			raise TypeError("size must be an integer")
 		elif value < 0:
@@ -32,7 +47,10 @@ class Square:
 		self.__size = value
 
 	def area(self):
-		""" public function to calculate the area of the square """
+		"""Calculates the area of square.
+
+		Returns: the current square area.
+		"""
 		res = self.__size ** 2
 		return res
 
