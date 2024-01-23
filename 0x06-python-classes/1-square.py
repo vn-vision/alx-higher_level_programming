d00d405832eee5be766fbda1babd93d7d16396c4
vn-vision <vctrngang@gmail.com>
2024-01-23 22:44:04 +0300

    private instances

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" This module contains a class Square based on (0-square.py) """
+
+class Square:
+    """ the square class attributes """
+    def __init__(self, size):
+        self.__size = size
