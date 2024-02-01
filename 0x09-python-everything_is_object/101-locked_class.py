a93cafa2dc34790923897d7c42f55a832f7a1efd
vn-vision <vctrngang@gmail.com>
2024-02-01 06:08:32 +0300

    Everything is an object advanced

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
 class LockedClass:
-    def __setatrr__(self, name, value):
-        if name == "first_name":
+    def __setattr__(self, name, value):
+        if name == 'first_name':
             self.__dict__[name] = value
         else:
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
