1e949229c08e9b2dbd4fb65fd7d07827fb95cea7
vn-vision <vctrngang@gmail.com>
2024-02-07 02:35:19 +0300

    save, load__, add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,25 +3,19 @@
 Module with a script that adds arguments
 to a Python list saved in a file
 """
-
 import sys
-import os
 
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
-if os.path.exists(filename):
-    # if it exists, called it, otherwise
-    # make a new one
-    the_list = load_from_json_file(filename)
-else:
-    with open(filename, mode='w') as f:
-        the_list = []
-        save_to_json_file([], filename)
+if len(sys.arg) > 1:
+    try:
+        items = load(filename)
+    except FileNotFoundError:
+        items = []
 
-for arg in range(1, len(sys.argv)):
-    the_list.append(sys.argv[arg])
-    save_to_json_file(the_list, filename)
+    items.extend(sys.arg[1:])
+    save(items, filename)
