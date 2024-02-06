7be626c1babc61a7cb7feec5fcf5b8f5d06e2b8f
vn-vision <vctrngang@gmail.com>
2024-02-07 02:29:16 +0300

    save, load, add

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,4 +7,5 @@ def load_from_json_file(filename):
     """ read from file and convert to obj"""
 
     with open(filename, 'r', encoding='utf-8') as f:
-        return json.loads(f)
+        data = f.read()
+        return json.loads(data)
