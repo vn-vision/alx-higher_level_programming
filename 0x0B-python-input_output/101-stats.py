e1c58a64dc97b61a75c21fee5e8d5dc19a477b32
vn-vision <vctrngang@gmail.com>
2024-02-07 21:17:09 +0300

    advancedCODE p

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,7 +5,7 @@ import sys
 
 
 tt_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_cnt = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 1
 
 try:
@@ -19,23 +19,20 @@ try:
             f_size = int(token[-1])
             tt_size += f_size
 
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
+            if status_code in status_cnt:
+                status_cnt[status_code] += 1
 
         if line_count % 10 == 0:
             print("File size: {}".format(tt_size))
-            for code in sorted(status_code_counts.keys()):
-                if status_code_counts[code] > 0:
-                    print("{}: {}".format(code, status_code_counts[code]))
-
-            tt_size = 0
-            status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+            for code in sorted(status_cnt.keys()):
+                if status_cnt[code] > 0:
+                    print("{}: {}".format(code, status_cnt[code]))
 
 except KeyboardInterrupt:
     """ this handles keyboard interractions """
 
     print("File size: {}".format(tt_size))
-    for code in sorted(status_code_counts.keys()):
-        if status_code_counts[code] > 0:
-            print("{}: {}".format(code, status_code_counts[code]))
+    for code in sorted(status_cnt.keys()):
+        if status_cnt[code] > 0:
+            print("{}: {}".format(code, status_cnt[code]))
     sys.exit(0)
