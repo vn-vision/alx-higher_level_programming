f71b020b57ee1e92341c2d8e12d732ef453c32b1
vn-vision <vctrngang@gmail.com>
2024-03-13 00:57:00 +0300

    Rows

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- this scripts shows the description of the table `first_table`
+-- It achieves this by displaying the `CREATE TABLE` stmt 
+-- that was used for `first_tabble`
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- list all rows of the table from the database
+SELECT COUNT(*) FROM first_table;
