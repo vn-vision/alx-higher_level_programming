33be1d84e73101915d2b7a02246be028875f2ebd
vn-vision <vctrngang@gmail.com>
2024-03-14 02:18:35 +0300

    Reload after grants

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- the user has all privileges -- password set to user_0d_pwd
 -- if user already exists return
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- don't forget to reload grant tables to ensure new images are in effect
 FLUSH PRIVILEGES;
