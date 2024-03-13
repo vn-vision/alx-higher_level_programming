ac0c4e4b6a9808020debb65051232d420dd685af
vn-vision <vctrngang@gmail.com>
2024-03-14 01:57:18 +0300

    GRANT

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- this script lists all the privileges of the MySQL users
+-- user_0d_1 and user_0d_2
+-- use show to display all grants 'privileges'
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+
+-- The display for the next user privileges
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
