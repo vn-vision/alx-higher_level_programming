2f8d543aeb1187eeccd864a0a00991d0b387f32f
vn-vision <vctrngang@gmail.com>
2024-03-14 02:16:01 +0300

    Reload after grants

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,6 @@
 -- if user already exists return
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- don't forget to reload grant tables to ensure new images are in effect
+FLUSH PRIVILEGES;
