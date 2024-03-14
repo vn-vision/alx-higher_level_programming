06415c741d843233c58096a98a1da7998f00c18d
vn-vision <vctrngang@gmail.com>
2024-03-14 13:09:12 +0300

    JOINING Tables INNER ee

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- import the database dump from htbtn_0d_tvshows to yout MySQL
 -- "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql" -s | mysql -uroot -p hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script that lists all the cities of California in database hbtn_0d_usa
 -- the states table contain one record where name = California
-SELECT * FROM cities WHERE state_id IN (SELECT id FROM states WHERE name='California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name='California') ORDER BY id ASC;
