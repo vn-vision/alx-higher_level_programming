f94e86dbf8776fc26cfe3a0701105b5b2179df24
vn-vision <vctrngang@gmail.com>
2024-03-14 10:33:28 +0300

    Second trial e

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- this script creates user user_0d_1
--- the user has all privileges -- password set to user_0d_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+-- the user has all privileges -- password set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- remember to reload the grant tables after creating new users
 FLUSH PRIVILEGES;
