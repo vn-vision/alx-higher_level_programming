5d8d9656a9a48317d1f27760852011306b3806d6
vn-vision <vctrngang@gmail.com>
2024-03-14 13:00:34 +0300

    JOINING Tables INNER d

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@
 USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 -- only use one SELECT, select id and name columns from ciites table
 -- name column from states table join them using state_id
 SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa.cities INNER JOIN hbtn_0d_usa.states ON
+FROM hbtn_0d_usa.cities JOIN hbtn_0d_usa.states ON
 cities.state_id = states.id ORDER BY cities.id ASC;
