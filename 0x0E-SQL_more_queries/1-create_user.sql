fd4a4bff2cb6130d37ea48bae97f72e60e0b43f5
vn-vision <vctrngang@gmail.com>
2024-03-14 10:27:04 +0300

    Second trial

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,4 @@
 -- if user already exists return
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- don't forget to reload grant tables to ensure new images are in effect
 FLUSH PRIVILEGES;
