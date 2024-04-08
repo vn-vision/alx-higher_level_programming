3ddfc12ce5fe9120eb172b46fa55618d90e574de
vn-vision <vctrngang@gmail.com>
2024-04-08 16:09:39 +0300

    Javascript programming, getting better

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,12 @@
 let big = 0;
 const len = process.argv.slice(2);
 
+if (len && len > 1) {
 // convert arguments to numbers
-const numbers = len.map(Number);
+  const numbers = len.map(Number);
 
-numbers.sort((a, b) => a - b);
+  numbers.sort((a, b) => a - b);
 
-big = numbers[numbers.length - 2];
-
+  big = numbers[numbers.length - 2];
+}
 console.log(big);
