2af7a7f40a9f566a9cf77c658770e9e2d994702d
vn-vision <vctrngang@gmail.com>
2024-04-12 21:51:26 +0300

    Trial and error again

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,26 +5,37 @@ import sys
 
 n = len(sys.argv)
 
-if n < 3:
+if n != 4:
     print("<usage: file.py username, password, database name")
+    sys.exit(1)
 
-USERNAME = sys.argv[0]
-PASS = sys.argv[1]
-MY_DB = sys.argv[2]
-MY_HOST = "http://localhost:3306"
+USERNAME = sys.argv[1]
+PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+MY_HOST = "localhost"
 
-''' connecting to the database on port 3306 with the provide credentials
-as well as the database to interact with '''
+'''
+connecting to the database on port 3306 with the provide credentials
+as well as the database to interact with
+'''
+try:
+    db = MySQLdb.connect(host=MY_HOST, port=3306,
+                         user=USERNAME, passwd=PASS, db=MY_DB)
 
-db = MySQLdb.connect(host=MY_HOST, user=USERNAME, passwd=PASS, db=MY_DB)
+    # get the cursor: givrs ability to have multiple separate working
+    # enironments through the same connection to db
 
-# get the cursor: givrs ability to have multiple separate working
-# enironments through the same connection to db
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id")
+    res = cur.fetchall()
 
-res = cur.fetchall()
+    for x in res:
+        print(x)
 
-for x in res:
-    print(x)
+    cur.close()
+    db.close()
+
+except MySQLdb.Error as e:
+    print("Error connecting to MySQL BD: {}".format(e))
+    sys.exit(1)
