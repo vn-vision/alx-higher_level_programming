f2e487f40bfe45089538e5feb82cd00fb1f19f1b
vn-vision <vctrngang@gmail.com>
2024-04-12 20:56:08 +0300

    Trial and error

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# connecting MySQL database
+import MySQLdbi
+import sys
+
+n = len(sys.argv)
+
+if n < 3:
+    print("<usage: file.py username, password, database name")
+
+USERNAME = sys.argv[0]
+PASS = sys.argv[1]
+MY_DB = sys.argv[2]
+MY_HOST = "http://localhost:3306"
+
+''' connecting to the database on port 3306 with the provide credentials 
+as well as the database to interact with '''
+
+db = MySQLdb.connect(host=MY_HOST, user=USERNAME, passwd=PASS, db=MY_DB)
+
+# get the cursor: givrs ability to have multiple separate working 
+# enironments through the same connection to db
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id")
+
+res = cur.fetchall()
+
+for x in res:
+    print(x)
+
+
