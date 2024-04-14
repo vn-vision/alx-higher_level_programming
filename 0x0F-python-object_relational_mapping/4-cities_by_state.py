f8e72e4cabde59aa5114744145106f3fd360e329
vn-vision <vctrngang@gmail.com>
2024-04-14 23:09:41 +0300

    Cities and States name instead of their IDs using INNER JOIN

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=USERNAME,
                          passwd=PASSWORD, db=DB, port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities
+                INNER JOIN states ON states.id=cities.state_id
+                ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for x in rows:
         print(x)
