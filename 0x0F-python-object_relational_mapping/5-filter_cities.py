6292b8d9939ecf51fd8607570637bc46b4e610cb
vn-vision <vctrngang@gmail.com>
2024-04-14 23:25:52 +0300

    Cities and States With JOIN

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-''' Script takes in an argument
-displays all values in the states table
-where name matches the argument
-compared to the script 2, it is safe from SQL injection
-'''
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-
-    USER = sys.argv[1]
-    PASS = sys.argv[2]
-    DB = sys.argv[3]
-    STATE = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=USER,
-                         passwd=PASS, db=DB)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities WHERE name LIKE %s", (STATE, ))
-
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
     cur.close()
     db.close()
