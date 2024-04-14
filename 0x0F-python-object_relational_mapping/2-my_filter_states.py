b5884b1c90f5d4b707a9fa8f02621ccefdaf037c
vn-vision <vctrngang@gmail.com>
2024-04-14 21:54:03 +0300

    Filter WHERE name LIKE user_specified  added BINARY(case sensitive)

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=USER,
                          passwd=PASS, db=DB)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'".format(STATE))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(STATE))
 
     rows = cur.fetchall()
 
