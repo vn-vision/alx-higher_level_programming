89a15220933c9059d7f621b2b4b84af9573f06dd
vn-vision <vctrngang@gmail.com>
2024-04-14 20:48:03 +0300

    Redeem MySQL

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
-# connecting MySQL database
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    n = len(sys.argv)
-
-    if n != 4:
-        print("<usage: file.py username, password, database name")
-        sys.exit(1)
-
-    USERNAME = sys.argv[1]
-    PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
-    MY_HOST = "localhost"
-
-    '''
-    connecting to the database on port 3306 with the provide credentials
-    as well as the database to interact with
-    '''
-     db = MySQLdb.connect(host=MY_HOST, port=3306,
-                             user=USERNAME, passwd=PASS, db=MY_DB)
-
-    # get the cursor: givrs ability to have multiple separate working
-    # enironments through the same connection to db
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-
-    res = cur.fetchall()
-
-    for x in res:
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for x in rows:
         print(x)
-
     cur.close()
     db.close()
