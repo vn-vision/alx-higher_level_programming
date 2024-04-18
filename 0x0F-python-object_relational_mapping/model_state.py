2f40bfede7cf5721432154e45d1ab67f38c6bdd3
vn-vision <vctrngang@gmail.com>
2024-04-18 17:19:43 +0300

    connecting to the database -- this time documented

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,14 +11,18 @@ Base = declarative_base()
 
 
 class State(Base):
-    # class state that inherits from Base
+    '''
+    This is  class state that inherits from Base
+    creates a table states and its attributes
+    id: pk, name: of the state
+    '''
     __tablename__ = 'states'
 
     # create a class attribute for the table states
     id = Column(Integer, primary_key=True, nullable=False, autogincrement=True)
     name = Column(String(128), nullable=False)
 
-'''
+
 user = sys.argv[1]
 passwd = sys.argv[2]
 db = sys.argv[3]
@@ -27,4 +31,3 @@ db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             user, passwd, db)
 engine = create_engine(db_url)
 Base.metadata.create_all(engine)
-'''
