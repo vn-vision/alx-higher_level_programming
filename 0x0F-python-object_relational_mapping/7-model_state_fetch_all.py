457fc363d89dcb8677d8c931b730a2b640a46add
vn-vision <vctrngang@gmail.com>
2024-04-18 17:46:55 +0300

    Not that simple apparently

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,8 +4,20 @@ this script fetches all the state data from specified db
 '''
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import create_engine
+import sys
 
-Session = sessionmaker()
 
-for instance in session.query(State).order_by(states.id):
-    print(instance.id, ": ", instance.name)
+if __name__ = "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(user, passwd, db))
+
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+
+    for instance in session.query(State).order_by(states.id):
+        print(instance.id, ": ", instance.name)
