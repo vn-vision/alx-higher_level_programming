555916cb34d32a72172543da3453eb1d4b7ac8a1
vn-vision <vctrngang@gmail.com>
2024-04-18 09:21:30 -0700

    Old fashioned way, sys.argv[]

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-'''
-this script fetches all the state data from specified db
-'''
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from sqlalchemy import create_engine
+"""Start link class to table in database
+"""
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(user, passwd, db))
-
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, ": ", instance.name)
+        print(instance.id, instance.name, sep=": ")
