096ccef6f973fc07bcf3ae7cbbc819c5e6762565
vn-vision <vctrngang@gmail.com>
2024-04-25 13:19:17 +0300

    The response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,17 +2,4 @@
 # This script takes in a url, sends GET request to the url
 # displays the body only if the status code is 200 ok
 
-if [ "$#" -ne 1  ]; then
-	echo "Usage:<file><url>"
-else
-	# extract the http_code response and the last 3 outputs
-	response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-	status_code=${response: -3}
-
-	if [ "$status_code" = "200" ]; then
-		# if the status code is 200 ok, display the output
-		curl -s "$1"
-	else
-		echo "Error: HTTP status_code: $status_code"
-	fi
-fi
+curl -sL "$1"
