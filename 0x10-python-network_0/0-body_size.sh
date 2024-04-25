40354f9d474e869aae7fb98a46432cd7064606c3
vn-vision <vctrngang@gmail.com>
2024-04-25 13:16:00 +0300

    Bash size wc vs sed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,5 @@
 # This script displays the content-leght of the response 
 # It takes url as a parameter
 
-curl -s "$1" | grep Content-Length | sed 's/[^0-9]*//g'
+# curl -s "$1" | grep Content-Length | sed 's/[^0-9]*//g'
+curl -s "$1" | wc -c
