cc74cc2a2d3fbb24dcfd2fe232cc2124e4277c14
vn-vision <vctrngang@gmail.com>
2024-04-25 14:26:18 +0300

    Follow the 3 line rule

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This sends request and displays the body only if the status code is 200 ok
-curl -sL -I "$1"
+curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a url, sends DELETE request to the url
-curl -X DELETE -s -I "$1"
+curl -s -I -X DELETE "$1"
