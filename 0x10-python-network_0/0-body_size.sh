9cf4cbe6e4ebb4b7fe3d736a56c83370ea4380ad
vn-vision <vctrngang@gmail.com>
2024-04-25 13:10:37 +0300

    Body Size Tena

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # This script displays the content-leght of the response 
 # It takes url as a parameter
 
-curl -s -I "$1" | grep Content-Length | sed 's/[^0-9]*//g'
+curl -s "$1" | grep Content-Length | sed 's/[^0-9]*//g'
