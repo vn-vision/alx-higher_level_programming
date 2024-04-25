5d34a117889b0e32790efaaba9535fde8bbde209
vn-vision <vctrngang@gmail.com>
2024-04-25 14:21:39 +0300

    Follow the 3 line rule

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # This script takes in a url, sends DELETE request to the url
-# displays the body only of the response
-
-if [ "$#" -ne 1  ]; then
-	echo "Usage:<file><url>"
-else
-	curl -X DELETE -s -I "$1"
-fi
+curl -X DELETE -s -I "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
-# This script takes in a url, sends GET request to the url
 # displays the HHTP methods the server accepts
-
-# Check if the script is called with exactly one argument (the URL)
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1  # Exit with an error status
-fi
-
-# Store the URL provided as the first argument
-url="$1"
-
-# Use curl to send a GET request (-I option sends a HEAD request)
-# to fetch the headers of the URL and then filter the 'Allow' header
-# which specifies the HTTP methods accepted by the server
-allowed_methods=$(curl -s -I "$url" | grep -i "^Allow:" | tr -d '\r' | cut -d ' ' -f 2-)
-
-# Check if the 'Allow' header was found
-if [ -n "$allowed_methods" ]; then
-    echo "Allowed HTTP methods for $url: $allowed_methods"
-else
-    echo "Unable to determine allowed HTTP methods for $url"
-fi
-
+curl -s -I "$1" | grep -i "^Allow:" | tr -d '\r' | cut -d ' ' -f 2-
