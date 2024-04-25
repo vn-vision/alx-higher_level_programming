f5b8a033e16b46689a31eb557200e9f6101f5a04
vn-vision <vctrngang@gmail.com>
2024-04-25 13:12:56 +0300

    shebang: /bin/bash

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script displays the content-leght of the response 
 # It takes url as a parameter
 
-curl -s "$1" | grep Content-Length | sed 's/[^0-9]*//g'
+curl -s -I "$1" | grep Content-Length | sed 's/[^0-9]*//g'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script takes in a url, sends GET request to the url
 # displays the body only if the status code is 200 ok
 
