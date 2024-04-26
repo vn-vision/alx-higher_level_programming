7eba3ece3da572ffedca3cb85adb06a95d1313c6
vn-vision <vctrngang@gmail.com>
2024-04-26 19:41:43 +0300

    Post some data

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,7 +7,9 @@ displays the value of variable X-Request-Id
 import requests
 import sys
 
-url = sys.argv[1]
-req = requests.get(url)
 
-print(req.headers['X-Request-Id'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+
+    print(req.headers['X-Request-Id'])
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+sends a POST request to passed URL with email
+displays the body response
+'''
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    req = requests.post(url, data={"email": email})
+    print(req.text)
