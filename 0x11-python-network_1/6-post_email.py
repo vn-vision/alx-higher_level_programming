a36cbf7451356bb7674800c225243d2bba3e0967
vn-vision <vctrngang@gmail.com>
2024-04-26 20:11:59 +0300

    moving on not working

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    requests.post(url, data={"email": email})
-    r = requests.get(url)
+    r = requests.post(url, data={"email": email})
     print(r.content)
