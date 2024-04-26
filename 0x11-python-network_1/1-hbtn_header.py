82854b296232bd68fecdd13f9f8ab1a90bd5bafa
vn-vision <vctrngang@gmail.com>
2024-04-26 15:41:33 +0300

    My header content

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,8 @@
 '''
 this script takes in a URL and displays the value of X-Request-Id
 found in header of the response
+make sure to print the content inside the with block
+it ensures the _id is the expected ID
 '''
 import urllib.request
 import sys
@@ -10,5 +12,4 @@ url = sys.argv[1]
 req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as response:
     _id = response.headers.get('X-Request-Id')
-
-print(_id)
+    print(_id)
