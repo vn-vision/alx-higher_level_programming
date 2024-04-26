f01fceaba91d703c198242db0bd58296e756b081
vn-vision <vctrngang@gmail.com>
2024-04-26 16:07:14 +0300

    POST Some Data. __name__ == __main__

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,12 +10,13 @@ data.encode - type of encoding that you want on the data to send
 import urllib
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode("utf-8")
-req = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode({'email': email}).encode("utf-8")
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-    html = response.read().decode('utf-8')
-    print('Your email is: ', html)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
+        print('Your email is: ', html)
