17ca81f6c0c9c0c724b70bb4f835a6bccc907c54
vn-vision <vctrngang@gmail.com>
2024-04-26 21:03:53 +0300

    api style

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     email = sys.argv[2]
 
     r = requests.post(url, data={"email": email})
-    print(r.content)
+    print(r.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user as a parameter
+'''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+
+    try:
+        val = sys.argv[1]
+    except IndexError:
+        val = ""
+
+    payload = {"q": val}
+    req = requests.post(url, params=payload)
+    try:
+        Data = req.json()
+
+        if Data:
+            print("[{}] {}".format(Data.id, Data.name))
+        else:
+            print("Not result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
